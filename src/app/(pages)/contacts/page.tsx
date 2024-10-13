c5cd572d784b28454f44c255b9613a2b50b2008f
Fedor Burhonov <fedor.burgonov@mdfin.com>
2024-10-13 22:20:37 +0300

    Hot fix

--- src/app/(pages)/contacts/page.tsx
+++ src/app/(pages)/contacts/page.tsx
@@ -2,30 +2,44 @@ import classNames from 'classnames';
 
 import style from './index.module.scss';
 
-const { BASE_URL } = process.env;
+// const { BASE_URL } = process.env;
 
 export default async function Page() {
-  const fetchData = await fetch(`${BASE_URL}/contacts/api`, {
+  /* const fetchData = await fetch(`${BASE_URL}/contacts/api`, {
     method: 'POST',
-  });
-  const { status, data } = await fetchData.json();
+  }); */
+  // const { status, data } = await fetchData.json();
 
-  if (status == 200) {
-    return (
-      <>
-        <h1 className={classNames(style.title)}>{data.title}</h1>
-        <div className={classNames(style.content)}>
-          {data.contacts.map(
-            ({ name, email }: { name: string; email: string }) => (
-              <p key={email}>
-                {name}: <a href={`mailto:${email}`}>{email}</a>
-              </p>
-            )
-          )}
-        </div>
-      </>
-    );
-  }
+  const data = {
+    title: 'Контакти',
+    contacts: [
+      {
+        name: 'Tsytok Ruslan',
+        email: 'tsrusp@gmail.com',
+      },
+      {
+        name: 'Burhonov Fedir',
+        email: 'fed-@ukr.net',
+      },
+    ],
+  };
 
-  return null;
+  // if (status == 200) {
+  return (
+    <>
+      <h1 className={classNames(style.title)}>{data.title}</h1>
+      <div className={classNames(style.content)}>
+        {data.contacts.map(
+          ({ name, email }: { name: string; email: string }) => (
+            <p key={email}>
+              {name}: <a href={`mailto:${email}`}>{email}</a>
+            </p>
+          )
+        )}
+      </div>
+    </>
+  );
+  // }
+
+  // return null;
 }
