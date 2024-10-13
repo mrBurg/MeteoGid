4b9136c7e34d7bd998f0ed40e3f4c528a7c3c4a7
Fedor Burhonov <fedor.burgonov@mdfin.com>
2024-10-13 22:28:42 +0300

    Added about us page API

--- src/app/(pages)/about-us/page.tsx
+++ src/app/(pages)/about-us/page.tsx
@@ -2,35 +2,26 @@ import classNames from 'classnames';
 
 import style from './index.module.scss';
 
-// const { BASE_URL } = process.env;
+const { BASE_URL } = process.env;
 
 export default async function Page() {
-  /* const fetchData = await fetch(`${BASE_URL}/about-us/api`, {
+  const fetchData = await fetch(`${BASE_URL}/about-us/api`, {
     method: 'POST',
-  }); */
-  // const { status, data } = await fetchData.json();
+  });
+  const { status, data } = await fetchData.json();
 
-  const data = {
-    title: 'Про нас',
-    text: [
-      '<b>MeteoGid</b> – додаток призначений для персоналізованого прогнозування погоди, надаючи користувачам індивідуальні рекомендації щодо погодних умов з акцентом на push - сповіщення.',
-      'Додаток допомагає користувачам планувати свій день, вдягатися відповідно до погоди, та завчасно реагувати на важливі зміни (опади, сильний вітер, заморозки).',
-      'Крім того, користувачі зможуть отримувати погодні дані для різних країн та регіонів світу, включаючи прогнози на кілька днів, показники вологості, швидкості вітру, атмосферного тиску та інших параметрів.',
-    ],
-  };
+  if (status == 200) {
+    return (
+      <>
+        <h1 className={classNames(style.title)}>{data.title}</h1>
+        <div className={classNames(style.content)}>
+          {data.text.map((item: string, index: number) => (
+            <p key={index} dangerouslySetInnerHTML={{ __html: item }} />
+          ))}
+        </div>
+      </>
+    );
+  }
 
-  // if (status == 200) {
-  return (
-    <>
-      <h1 className={classNames(style.title)}>{data.title}</h1>
-      <div className={classNames(style.content)}>
-        {data.text.map((item: string, index: number) => (
-          <p key={index} dangerouslySetInnerHTML={{ __html: item }} />
-        ))}
-      </div>
-    </>
-  );
-  // }
-
-  // return null;
+  return null;
 }
